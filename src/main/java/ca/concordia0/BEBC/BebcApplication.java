e2a3153db9d14c8a254d408b5d9a909e85e70029
zsnejom <Jose.x.Mendes@assistera.com>
2019-11-02 15:56:23 -0400

    First commit.

---
+++ src/main/java/ca/concordia0/BEBC/BebcApplication.java
@@ -0,0 +1,13 @@
+package ca.concordia0.BEBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/concordia0/BEBC/BebcApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.concordia0.BEBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
