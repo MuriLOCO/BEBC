17aebc0203a7f4df66e9558f29aec8d1c6174449
zsnejom <Jose.x.Mendes@assistera.com>
2019-11-12 19:40:52 -0500

    Remove sintax error.

--- src/main/java/ca/concordia/BEBC/equations/EquationUtil.java
+++ src/main/java/ca/concordia/BEBC/equations/EquationUtil.java
@@ -8,38 +8,38 @@ import java.util.List;
 
 public class EquationUtil {
 
-    private static BigInteger calculateNc(BigInteger tr, BigInteger tint){
+    private static BigInteger calculateNc(BigInteger tr, BigInteger tint) {
         return tr.divide(tint);
     }
 
-    private static BigInteger calculateTw(BigInteger nc, BigInteger tr, BigInteger tint){
+    private static BigInteger calculateTw(BigInteger nc, BigInteger tr, BigInteger tint) {
         return nc.multiply(tint).subtract(tr);
     }
 
-    private static BigInteger calculateTrtot(BigInteger tr, BigInteger tw){
+    private static BigInteger calculateTrtot(BigInteger tr, BigInteger tw) {
         return tr.add(tw);
     }
 
-    private static BigInteger calculateEkmTot(BigInteger eavg, BigInteger btc, BigInteger bped, BigInteger phvacAvg, BigInteger tr, BigInteger d){
+    private static BigInteger calculateEkmTot(BigInteger eavg, BigInteger btc, BigInteger bped, BigInteger phvacAvg, BigInteger tr, BigInteger d) {
         BigInteger firstPart = eavg.add(new BigInteger("0.1").multiply(btc.divide(bped)));
         BigInteger secondPart = phvacAvg.multiply(tr.divide(new BigInteger("3600").divide(d)));
         return firstPart.add(secondPart);
     }
 
-    private static BigInteger calculatePchg(BigInteger emax, BigInteger d, BigInteger tw, BigInteger nchg){
+    private static BigInteger calculatePchg(BigInteger emax, BigInteger d, BigInteger tw, BigInteger nchg) {
         return new BigInteger("3600").multiply(emax).multiply(d).divide(tw.multiply(nchg));
     }
 
-    private static BigInteger calculateNbr(BigInteger ds, BigInteger bn, BigInteger btc, BigInteger ekmtot){
+    private static BigInteger calculateNbr(BigInteger ds, BigInteger bn, BigInteger btc, BigInteger ekmtot) {
         return ds.multiply(bn.multiply(btc).divide(ekmtot));
     }
 
-    private static BigInteger calculateCBus(BigInteger nc, BigInteger cv, BigInteger cb, BigInteger cc, BigInteger ce, BigInteger ds, BigInteger ekmTot, BigInteger cdem, BigInteger nbr, BigInteger ts, BigInteger dRate, BigInteger j){
+    private static BigInteger calculateCBus(BigInteger nc, BigInteger cv, BigInteger cb, BigInteger cc, BigInteger ce, BigInteger ds, BigInteger ekmTot, BigInteger cdem, BigInteger nbr, BigInteger ts, BigInteger dRate, BigInteger j) {
         BigInteger summationValue = new BigInteger("0");
         //Resolve summation
-        for(int i = 0; i < j.intValue(); i++){
-        BigInteger auxSummation = ce.multiply(ds.multiply(ekmTot)).add(cdem).add(cb.multiply(nbr.divide(ts)).multiply(new BigInteger("1").subtract(dRate).pow(-i)));
-        summationValue = summationValue.add(auxSummation);
+        for (int i = 0; i < j.intValue(); i++) {
+            BigInteger auxSummation = ce.multiply(ds.multiply(ekmTot)).add(cdem).add(cb.multiply(nbr.divide(ts)).multiply(new BigInteger("1").subtract(dRate).pow(-i)));
+            summationValue = summationValue.add(auxSummation);
         }
         return nc.multiply(cv.add(cb)).add(cc).add(summationValue).divide(nc);
     }
@@ -53,25 +53,26 @@ public class EquationUtil {
      * pchg -  charging power
      * nbr - number of battery replacements during a bus’s service life
      * ncbus - total cost of ownership of an individual bus
-     * @param tr - time spent driving
-     * @param tint - bus schedule interval
-     * @param eavg - average consumption (kWh/km)
-     * @param btc -battery capacity
-     * @param bped - battery pack energy density (Wh/kg)
+     *
+     * @param tr       - time spent driving
+     * @param tint     - bus schedule interval
+     * @param eavg     - average consumption (kWh/km)
+     * @param btc      -battery capacity
+     * @param bped     - battery pack energy density (Wh/kg)
      * @param phvacavg - average HVAC system (kW)
-     * @param d - distance travelled (round-trip)
-     * @param emax - maximum consumption (kWh/km)
-     * @param ncgh - charging efficiency
-     * @param ds - total distance travelled during service life
-     * @param bn - battery cycles
-     * @param cv - vehicle cost (k€)
-     * @param cb - battery cost (€/kWh)
-     * @param cc - charging station cost (€/kW)
-     * @param ce - electricity cost (€/kWh)
-     * @param cdem - demand cost ((€/kW)/y)
-     * @param ts - service life (y)
-     * @param drate - discounte rate (%)
-     * @param j - service years from first to last
+     * @param d        - distance travelled (round-trip)
+     * @param emax     - maximum consumption (kWh/km)
+     * @param ncgh     - charging efficiency
+     * @param ds       - total distance travelled during service life
+     * @param bn       - battery cycles
+     * @param cv       - vehicle cost (k€)
+     * @param cb       - battery cost (€/kWh)
+     * @param cc       - charging station cost (€/kW)
+     * @param ce       - electricity cost (€/kWh)
+     * @param cdem     - demand cost ((€/kW)/y)
+     * @param ts       - service life (y)
+     * @param drate    - discounte rate (%)
+     * @param j        - service years from first to last
      * @return
      */
     public static List<BigInteger> calculateAllValue(BigInteger tr, BigInteger tint, BigInteger eavg,
@@ -80,7 +81,7 @@ public class EquationUtil {
                                                      BigInteger ds, BigInteger bn, BigInteger cv,
                                                      BigInteger cb, BigInteger cc, BigInteger ce,
                                                      BigInteger cdem, BigInteger ts, BigInteger drate,
-                                                     BigInteger j){
+                                                     BigInteger j) {
 
         //Number of buses (nc)
         BigInteger nc = calculateNc(tr, tint);
@@ -106,11 +107,4 @@ public class EquationUtil {
         return Arrays.asList(nc, tw, tort, ekmtot, pchg, nbr, ncbus);
     }
 
-    public List<List<BigInteger>> calculateListOfAllValues(List<BigInteger> trs, BigInteger tint, BigInteger eavg,
-                                                           BigInteger btc, BigInteger bped, BigInteger phvacavg,
-                                                           BigInteger d, BigInteger emax, BigInteger ncgh,
-                                                           BigInteger ds, BigInteger bn, BigInteger cv,
-                                                           BigInteger cb, BigInteger cc, BigInteger ce,
-                                                           BigInteger cdem, BigInteger ts, BigInteger drate,
-                                                           BigInteger j)
 }
